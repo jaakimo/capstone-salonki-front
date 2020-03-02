7069eef89626119790d49abbeb9d6cfce34c945d
JaakkoK <jaska91@hotmail.com>
2020-03-02 13:26:44 +0200

    frontpage logos

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 function FrontPage(){
     return(
-        <div>
-            <h1>Tervetuloa smart salonkiin!</h1>
-            <p>frontpage placeholder</p>
-        </div>
+        <>
+            <div>
+                <h1>Tervetuloa smart salonkiin!</h1>
+                <p>frontpage placeholder</p>
+            </div>
+            <footer style={{"display":"block","bottom":"0","position":"absolute","left":"40%"}}>
+                <img src="./salo.jpg" height="100px"></img>
+                <img src="./utu.jpg" height="100px"></img>
+            </footer>
+        </>
     );
 }
 export default FrontPage;
