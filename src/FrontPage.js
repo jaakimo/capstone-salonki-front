0ab29e70a93393ea5a82484a6cf0dbe2cb322bf5
JaakkoK <jaska91@hotmail.com>
2020-03-02 14:08:57 +0200

    lounea logo

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -6,9 +6,10 @@ function FrontPage(){
                 <h1>Tervetuloa smart salonkiin!</h1>
                 <p>frontpage placeholder</p>
             </div>
-            <footer style={{"display":"block","bottom":"0","position":"absolute","left":"40%"}}>
+            <footer style={{"display":"block","bottom":"0","position":"absolute","left":"32%"}}>
                 <img src="./salo.jpg" height="100px"></img>
                 <img src="./utu.jpg" height="100px"></img>
+                <img src="./lounea.jpg" height="100px"></img>
             </footer>
         </>
     );
